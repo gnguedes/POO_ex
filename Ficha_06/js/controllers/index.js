ebfa34f7792c9c678666355cc378daa44495dd5b
gnguedes <goncalo.alt@gmail.com>
2019-04-12 17:04:50 +0100

    adicao de bandas

--- Ficha_06/js/controllers/index.js
+++ Ficha_06/js/controllers/index.js
@@ -1,36 +1,52 @@
 import { bands } from "../models/main.js"
 
+//butao filter
 const myCatalog = document.querySelector("#myCatalog")
 const btnFilter = document.querySelector("#btnFilter")
-btnFilter.addEventListener("click", function(){
+btnFilter.addEventListener("click", function(event){
     const txtName=document.querySelector("#txtName").value
     const sltGenre=document.querySelector("#sltGenre").value
 
-    renderCatalog(slt.genre,txtName)
+    renderCatalog(txtName,sltGenre)
+    event.preventDefault()
 })
+//butao remover
+const btnRemove = document.querySelector("#btnRemove")
+btnRemove.addEventListener("click",function (event) {
+    
+    
+})
+
+
+
 // teste -- myCatalog.innerHTML="dsc"
 
 renderCatalog()
 
-function renderCatalog(filterGenre = "", filterName = "") {
+function renderCatalog(txtName = "", sltGenre = "") {
     let result = ""
     let i = 0
     for (const band of bands) {
-        if(filterGenre!=="" && filterGenre==band.genre) 
-
-
+        if(
+            //filtro
+            (sltGenre!=="" && band.genre!==sltGenre)
+            ||
+            (txtName!=="" && !band.name.startsWith(txtName))
+            ) {
+            continue
+        } 
         if (i % 3 === 0) {
             result += `<div class="row">`
         }
-        result = `
+        result += `
         <div class="col-sm-4">
             <div class="card">
                 <img class="card-img-top" src="${band.photo}" alt="">
                 <div class="card-body">
-                    <h4 class="card-title">${band.name}</h4>
+                    <h5 class="card-title">${band.name}</h5>
                     <p class="card-text">${band.genre}</p>
-                    <a name="" id="" class="btn btn-sucess" href="#" >Ver Mais</a>
-                    <a name="" id="" class="btn btn-danger" href="#" >Remover</a>
+                    <a id="btnSeeMore" class="btn btn-success" href="#" >Ver Mais</a>
+                    <a id="btnRemove" class="btn btn-danger" href="#" >Remover</a>
     
                 </div>
             </div>
@@ -40,10 +56,13 @@ function renderCatalog(filterGenre = "", filterName = "") {
         if (i % 3 === 0) {
             result += `</div>`
         }
+        
     }
+    myCatalog.innerHTML = result
+    
 }
+
 
     
 
-        //myCatalog.innerHTML = result
-        myCatalog.innerHTML = bands.length
+        
--- Ficha_06/js/models/main.js
+++ Ficha_06/js/models/main.js
@@ -1,15 +1,48 @@
 import Band from "./band.js"
 import User from "./user.js"
 
+
 //Array para guardar os objetos band
-export const bands = []
 
-const band1 = new Band("Muse", "Pop-rock", "http://specials-images.forbesimg.com/imageserve/577febc24bbe6f31cdfaf49c/416x416.jpg?background=000000&cropX1=0&cropX2=744&cropY1=21&cropY2=765", "A great band!", "XXX")
-const band2 = new Band("Radiohead", "Pop-rock", "https://www.grammy.com/sites/com/files/styles/image_landscape_hero/public/radiohead_hero_688547436.jpg?itok=tcnCxZ6t", "A great band!", "XXX")
-const band3 = new Band("Metallica", "Metal", "https://images.impresa.pt/blitz/2016-08-19-metallica.jpg/original/mw-860", "A great band!", "XXX")
-const band4 = new Band("Artic Monkeys", "Pop-rock", "https://ep01.epimg.net/cultura/imagenes/2018/05/08/actualidad/1525777333_134713_1525778067_noticia_normal.jpg", "A great band!", "XXX")
+export let bands = []
 
-bands.push(band1,band2,band3,band4)
+if (localStorage.getItem("bands")) {
+    bands = JSON.parse(localStorage.getItem("bands"))
+} else {
+    const band1 = new Band("Muse", "Pop-rock", "http://specials-images.forbesimg.com/imageserve/577febc24bbe6f31cdfaf49c/416x416.jpg?background=000000&cropX1=0&cropX2=744&cropY1=21&cropY2=765", "A great band!", "XXX")
+    const band2 = new Band("Radiohead", "Pop-rock", "https://www.grammy.com/sites/com/files/styles/image_landscape_hero/public/radiohead_hero_688547436.jpg?itok=tcnCxZ6t", "A great band!", "XXX")
+    const band3 = new Band("Metallica", "Metal", "https://images.impresa.pt/blitz/2016-08-19-metallica.jpg/original/mw-860", "A great band!", "XXX")
+    const band4 = new Band("Artic Monkeys", "Pop-rock", "https://ep01.epimg.net/cultura/imagenes/2018/05/08/actualidad/1525777333_134713_1525778067_noticia_normal.jpg", "A great band!", "XXX")
+
+    bands.push(band1, band2, band3, band4)
+    localStorage.setItem("bands", JSON.stringify(bands))
+}
+
+
+
+
+export function addBand(txtName, sltGenre, txtDescription, txtPhoto, txtVideoclip) {
+    let existBand = false
+    for (const band of bands) {
+        if (band.name === txtName) {
+            existBand = true
+            break
+        }
+    }
+
+    if (existBand === false) {
+        bands.push(new Band(txtName, sltGenre, txtPhoto, txtDescription, txtVideoclip))
+        localStorage.setItem("bands", JSON.stringify(bands))
+        alert(`Banda ${txtName} adicionada com sucesso!`)
+        location.href = "/Ficha_06/index.html"
+    }
+    else {
+        alert(`Banda ${txtName} já existe!`)
+    }
+}
+
+
+
 
 //Array para guardar os objetos user
 export const users = []
@@ -18,4 +51,4 @@ const user1 = new User("Ricardo", "12345")
 const user2 = new User("Maria", "54321")
 const user3 = new User("Pedro", "15432")
 
-users.push(user1,user2,user3)
+users.push(user1, user2, user3)
